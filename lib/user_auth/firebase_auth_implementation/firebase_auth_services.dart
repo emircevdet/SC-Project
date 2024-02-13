f8a60b08ee074bb43fa0f444b3e7ab0ee34fe111
nefretac <karahanyavuz61@gmail.com>
2024-02-13 23:26:04 +0300

    firebase edits

--- lib/user_auth/firebase_auth_implementation/firebase_auth_services.dart
+++ lib/user_auth/firebase_auth_implementation/firebase_auth_services.dart
@@ -7,9 +7,36 @@ class FirebaseAuthService {
   Future<User?> signUpWithEmailAndPassword(String email, String password) async{
 
     try {
-      UserCredential credential = auth.createUserWithEmailAndPassword(email: email, password: password);
+      UserCredential credential =await _auth.createUserWithEmailAndPassword(email: email, password: password);
       return credential.user;
+    } catch (e) {
+     print("Some error occured");
     }
-
+    return null;
   }
+
+  Future<User?> signInWithEmailAndPassword(String email, String password) async{
+
+    try {
+      UserCredential credential =await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return credential.user;
+    } catch (e) {
+      print("Some error occured");
+    }
+    return null;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
